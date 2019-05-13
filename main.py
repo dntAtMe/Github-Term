51648cb287eaa0e9b61b1ea7ed49fa7605ff2850
dntAtMe <dntatm3@gmail.com>
2019-05-13 14:45:52 +0200

    config basis

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import pathlib
 
-# TODO: Create config if empty folder exists
-configFile = "config.json"
-path = pathlib.Path.home() / '.termhub'
-print(path)
-filepath = path / configFile
-if not path.exists():
-    path.mkdir()
-    filepath.touch()
-with filepath.open("r", encoding="utf-8") as f:
-    f.readable()
+
+class Config:
+    def __init__(self): pass
+
+    def load(self, json): pass
+
+
+def create_default_config_file():
+    pass
+
+
+def load_config():
+    pass
+
+
+def write_config(config, file):
+    pass
+
+
+def read_config_file(path=pathlib.Path.home() / '.termhub', config='config.json'):
+    if not path.exists():
+        path.mkdir()
+    filepath = path / config
+
+    if not filepath.exists():
+        create_default_config_file()
+        # TODO: Fix temporary solution
+        filepath.touch()
+
+    with filepath.open("r", encoding="utf-8") as file:
+        print(file.read())
+
+
+if __name__ == '__main__':
+    read_config_file()
